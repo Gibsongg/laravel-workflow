cb226309ddff08dc508315a636cc0b8f83094219
Sklyarov Roman <clouds@bk.ru>
2021-02-17 15:25:25 +0300

    Rename trait

--- src/Traits/WorkflowTrait.php
+++ src/Traits/WorkflowTrait.php
@@ -2,29 +2,42 @@
 
 namespace ZeroDaHero\LaravelWorkflow\Traits;
 
+use Symfony\Component\Workflow\Marking;
+use Symfony\Component\Workflow\Transition;
 use Workflow;
+use Symfony\Component\Workflow\Workflow as WorkflowSymfony;
 
 /**
  * @author Boris Koumondji <brexis@yahoo.fr>
  */
 trait WorkflowTrait
 {
-    public function workflow_apply($transition, $workflow = null)
+    public function workflowApply($transition, $workflow = null): Marking
     {
-        return Workflow::get($this, $workflow)->apply($this, $transition);
+        return $this->workflowGet($workflow)->apply($this, $transition);
     }
 
-    public function workflow_can($transition, $workflow = null)
+    public function workflowCan($transition, $workflow = null): bool
     {
-        return Workflow::get($this, $workflow)->can($this, $transition);
+        return $this->workflowGet($workflow)->can($this, $transition);
     }
 
-    public function workflow_transitions($workflow = null)
+    /**
+     *
+     * @param null $workflow
+     * @return Transition[]
+     */
+    public function workflowTransitions($workflow = null): array
     {
-        return Workflow::get($this, $workflow)->getEnabledTransitions($this);
+        return $this->workflowGet($workflow)->getEnabledTransitions($this);
     }
 
-    public function workflow_get($workflow = null)
+    public function workflowTransition($transitionName, $workflow = null): ?Transition
+    {
+        return $this->workflowGet($workflow)->getEnabledTransition($this, $transitionName);
+    }
+
+    public function workflowGet($workflow = null): WorkflowSymfony
     {
         return Workflow::get($this, $workflow);
     }
